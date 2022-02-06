3561006fc2a14666e7d296b4deb3bc0c099b3124
Jamie Howard <jamiehowardco@gmail.com>
2022-02-06 17:01:26 -0500

    Added caching to leaderboard.

--- app/Http/Controllers/DronieLeaderController.php
+++ app/Http/Controllers/DronieLeaderController.php
@@ -4,16 +4,21 @@ namespace App\Http\Controllers;
 
 use App\Models\Dronie;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 
 class DronieLeaderController extends Controller
 {
     public function index()
     {
         // Sort by dronies with the highest number of winning votes
-        $leaders = \App\Models\Dronie::withCount('winningVotes')
-            ->orderBy('winning_votes_count', 'desc')
-            ->get()
-            ->take(3);
+
+        $leaders = Cache::remember('leaderboard', 120, function() {
+            return \App\Models\Dronie::withCount('winningVotes')
+                ->orderBy('winning_votes_count', 'desc')
+                ->get()
+                ->take(3)
+                ->reverse();
+        });
 
         return response($leaders);
     }
