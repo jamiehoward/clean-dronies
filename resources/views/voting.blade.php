323addf220191477d5955305c0455a2c85c10a00
Jamie Howard <jamiehowardco@gmail.com>
2022-02-06 03:23:05 -0500

    Added some basic nav and cta

--- resources/views/voting.blade.php
+++ resources/views/voting.blade.php
@@ -19,13 +19,28 @@
     <body class="font-mono antialiased bg-black">
         <div class="w-full" id="app">
 
-            <div class="mx-auto w-1/2 mt-16 text-center p-8 border-green-300 border-dotted border-2">
+            <nav class="text-white  p-4 flex justify-between">
+                <div class="font-bold">
+                    <a href="/" class="hover:text-green-300 transition ease-in-out">
+                        {{config('app.name')}}
+                    </a>
+                </div>
+                <div><strike class="cursor-not-allowed">Connect Wallet</strike></div>
+            </nav>
+
+
+            <div class="mx-auto md:w-1/2 mt-16 text-center p-8 border-green-300 border-dotted border-2">
                 <h1 class="font-bold text-lg mb-8 text-white uppercase">Which Dronie is cleaner?</h1>
                 <dronie-voting-list></dronie-voting-list>
             </div>
 
             <dronie-leaderboard></dronie-leaderboard>
+            
+            <div class="text-white text-center mt-12">
+                Made with <span class="text-red-500">❤</span> by <a href="https://twitter.com/intent/follow?screen_name=JamieHoward" class="text-red-500">@JamieHoward</a>
+            </div>
         </div>
+
 
     </body>
 </html>
